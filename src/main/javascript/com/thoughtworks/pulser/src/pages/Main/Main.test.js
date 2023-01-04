7abbc1f1f76ae48384e3e0448af2775fb8584f9b
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-01-04 18:30:28 +0100

    Create and test the Header

--- src/main/javascript/com/thoughtworks/pulser/src/pages/Main/Main.test.js
+++ src/main/javascript/com/thoughtworks/pulser/src/pages/Main/Main.test.js
@@ -1,9 +1,25 @@
-import { render, screen } from "@testing-library/react";
-import Main from ".";
+import { cleanup, render, screen } from "@testing-library/react";
+import Main from "./index.jsx"
 
-test("renders Header", () => {
-  render(<Main />);
- 
- const headingElement = screen.getByRole("header");
-  expect(headingElement).toHaveTextContent("How are you feeling today?");
+
+describe("Main Page", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should contain header", () => {
+    render(<Main />);
+
+    const mainHeaderElement = screen.getByTestId("main-header");
+
+    expect(mainHeaderElement).toBeInTheDocument();
+  });
+
+  it("should render the", ()=>{
+    render(<Main/>);
+
+    const headerText = screen.getByText('How are you feeling today?');
+    
+    expect(headerText).toBeInTheDocument();
+  })
 });
--- src/main/javascript/com/thoughtworks/pulser/src/pages/Main/index.jsx
+++ src/main/javascript/com/thoughtworks/pulser/src/pages/Main/index.jsx
@@ -4,7 +4,7 @@ function Main() {
   return (
     <div className="Main">
       <header className="Main-header">
-       
+        <h1 data-testid="main-header"> How are you feeling today?  </h1>
       </header>
     </div>
   );
