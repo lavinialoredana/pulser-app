fdbb6c353c2dd06d4797d893f1753215f1d211f7
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-01-04 14:08:15 +0100

    Initial cleanup

--- src/main/javascript/com/thoughtworks/pulser/src/App.test.js
+++ src/main/javascript/com/thoughtworks/pulser/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Pulser Header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Pulser/i);
   expect(linkElement).toBeInTheDocument();
 });
