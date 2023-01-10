e35fcf365de16cfc51c37181ab8032deef971cc6
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-10 16:03:55 +0100

    PULSER-2: make time stamp not null

--- src/main/java/com/thoughtworks/pulser/model/Message.java
+++ src/main/java/com/thoughtworks/pulser/model/Message.java
@@ -1,12 +1,16 @@
 package com.thoughtworks.pulser.model;
 
+import java.time.LocalDateTime;
+
 public class Message {
   private Face face;
   private String body;
+  LocalDateTime creationTimeStamp;
 
   public Message(Face face, String body) {
     this.face = face;
     this.body = body;
+    this.creationTimeStamp = LocalDateTime.now();
   }
 
   public enum Face {
--- src/test/java/com/thoughtworks/pulser/model/MessageTest.java
+++ src/test/java/com/thoughtworks/pulser/model/MessageTest.java
@@ -3,6 +3,8 @@ package com.thoughtworks.pulser.model;
 import static org.testng.Assert.*;
 
 import com.thoughtworks.pulser.model.Message.Face;
+import java.time.LocalDateTime;
+import net.bytebuddy.asm.Advice.Local;
 import org.testng.annotations.Test;
 
 public class MessageTest {
@@ -13,8 +15,12 @@ public class MessageTest {
 
   @Test
   public void itShouldHaveFaceAndBody() {
-
     assertEquals(message.getFeedbackMessage(), "AWESOME" + " " + "This is a COMPLETE feedback message");
+  }
+
+  @Test
+  public void itShouldHaveCreationTimeStamp() {
+    assertNotNull(message.creationTimeStamp);
   }
 
 }
