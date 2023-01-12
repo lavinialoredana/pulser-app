64df847f668c5f2a5808a85a23a0b266da067404
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-12 11:56:40 +0100

    PULSER-2: add message repository

---
+++ src/main/java/com/thoughtworks/pulser/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.pulser.repository;
+
+import com.thoughtworks.pulser.model.Message;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends MongoRepository<Message, ObjectId> {
+
+}
