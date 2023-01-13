5b9dd97a0e27f611420fc9a568b2b5c02acc24c2
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-13 12:55:42 +0100

    PULSER-2: refactor face type

--- src/test/java/com/thoughtworks/pulser/service/MessageServiceTest.java
+++ src/test/java/com/thoughtworks/pulser/service/MessageServiceTest.java
@@ -5,7 +5,6 @@ import static org.mockito.Mockito.when;
 import static org.testng.Assert.*;
 
 import com.thoughtworks.pulser.model.Message;
-import com.thoughtworks.pulser.model.Message.Face;
 import com.thoughtworks.pulser.repository.MessageRepository;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -26,10 +25,10 @@ public class MessageServiceTest {
   @BeforeMethod
   void setUp() {
     messageRepository = mock(MessageRepository.class);
-    message = new Message(Face.HAPPY, "my message");
-    message2 = new Message(Face.AWESOME, "my second message");
-    emptyBodyMessage = new Message(Face.AWESOME, null);
-    messageLongBody = new Message(Face.AWESOME, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ultricies ante non malesuada. Fusce ullamcorper interdum augue, et laoreet dolor euismod ut. Ut dictum ante eros, et dapibus urna vestibulum vel. Suspendisse eget diam mi. Donec consequat diam justo, eu interdum nisi laoreet elementum. Mauris consequat suscipit viverra. Suspendisse ac porta neque.Pellentesque aliquam vel metus sagittis blandit. Vivamus ut ipsum vel ex efficitur vestibulum eget ac felis. Vestibulum sagittis ac lacus sed tristique. Integer vulputate orci ut ornare commodo. Morbi eleifend consequat euismod. Sed sagittis leo enim, sit amet porttitor quam auctor in. Sed quis ultrices leo. Nullam posuere fringilla tortor, in aliquet urna viverra convallis. Integer sollicitudin nunc vel nisi placerat, non vestibulum lacus rutrum. Proin venenatis metus sit amet fermentum ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed maximus cursus massa, vitae ullamcorper ipsum fermentum eget. Sed fermentum massa eleifend scelerisque tempor.Nullam commodo eget dolor eget ultricies. Suspendisse at ornare tellus. Sed nec tincidunt erat, eget tincidunt tortor. Sed efficitur turpis nec diam iaculis, a volutpat purus tristique. Suspendisse et iaculis mi. Pellentesque semper lorem eget tortor vulputate, a dignissim nulla efficitur. Phasellus eu risus iaculis nullamj.");
+    message = new Message("HAPPY", "my message");
+    message2 = new Message("AWESOME", "my second message");
+    emptyBodyMessage = new Message("AWESOME", null);
+    messageLongBody = new Message("AWESOME", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ultricies ante non malesuada. Fusce ullamcorper interdum augue, et laoreet dolor euismod ut. Ut dictum ante eros, et dapibus urna vestibulum vel. Suspendisse eget diam mi. Donec consequat diam justo, eu interdum nisi laoreet elementum. Mauris consequat suscipit viverra. Suspendisse ac porta neque.Pellentesque aliquam vel metus sagittis blandit. Vivamus ut ipsum vel ex efficitur vestibulum eget ac felis. Vestibulum sagittis ac lacus sed tristique. Integer vulputate orci ut ornare commodo. Morbi eleifend consequat euismod. Sed sagittis leo enim, sit amet porttitor quam auctor in. Sed quis ultrices leo. Nullam posuere fringilla tortor, in aliquet urna viverra convallis. Integer sollicitudin nunc vel nisi placerat, non vestibulum lacus rutrum. Proin venenatis metus sit amet fermentum ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed maximus cursus massa, vitae ullamcorper ipsum fermentum eget. Sed fermentum massa eleifend scelerisque tempor.Nullam commodo eget dolor eget ultricies. Suspendisse at ornare tellus. Sed nec tincidunt erat, eget tincidunt tortor. Sed efficitur turpis nec diam iaculis, a volutpat purus tristique. Suspendisse et iaculis mi. Pellentesque semper lorem eget tortor vulputate, a dignissim nulla efficitur. Phasellus eu risus iaculis nullamj.");
     when(messageRepository.save(message)).thenReturn(message);
     when(messageRepository.save(message2)).thenReturn(message2);
     when(messageRepository.findAll()).thenReturn(List.of(message, message2));
