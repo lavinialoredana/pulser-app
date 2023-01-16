8969950aff29b09fc03e8ff7c019a815484ab9f2
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-16 17:27:34 +0100

    PULSER-2: remove extra constructor by using lombok annotation. Rename parameters for readability. Change Id type. Remove repository.

--- src/main/java/com/thoughtworks/pulser/controller/MessageController.java
+++ src/main/java/com/thoughtworks/pulser/controller/MessageController.java
@@ -1,13 +1,10 @@
 package com.thoughtworks.pulser.controller;
 
 import com.thoughtworks.pulser.model.Message;
-import com.thoughtworks.pulser.repository.MessageRepository;
 import com.thoughtworks.pulser.service.MessageService;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
-import org.bson.types.ObjectId;
-import org.springframework.http.HttpStatus;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,33 +17,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
 
-  private final MessageRepository messageRepository;
   private final MessageService messageService;
 
-  //TODO @Rebeca: Lombok library can save you from manually creating constructors
-  //TODO @Rebeca: A controller does never know about repositories, only about services.
-  // Remove any repo reference from this class (of course doing TDD)
-  public MessageController(MessageRepository messageRepository, MessageService messageService) {
-    this.messageRepository = messageRepository;
-    this.messageService = messageService;
-  }
-
-  //TODO @Rebeca: Review the naming: createMessage is a more clear name for this endpoint.
-  // Also, newMessage is a weaker name than just message. Review naming in general
   @PostMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
-  public @ResponseBody ResponseEntity<Message> addNewMessage(@RequestBody Message newMessage) {
-    //TODO @Rebeca: I don't think there is a need of try-catching here
-    Message savedMessage;
-    try {
-      savedMessage = messageService.saveMessage(newMessage);
-    } catch (InputMismatchException e) {
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-    return new ResponseEntity<>(savedMessage, HttpStatusCode.valueOf(200));
+  public @ResponseBody ResponseEntity<Message> createMessage(@RequestBody Message message) {
+    return new ResponseEntity<>(messageService.saveMessage(message), HttpStatusCode.valueOf(200));
   }
 
   @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -56,18 +36,17 @@ public class MessageController {
 
   @GetMapping(value = "/message/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public @ResponseBody Optional<Message> findMessage(@PathVariable String id) {
-    return messageService.findMessageById(new ObjectId(id));
+    return messageService.findMessageById(id);
   }
 
-  //TODO @Rebeca: deleteAllMessageS
   @RequestMapping(value = "/delete/all", method = RequestMethod.DELETE)
-  public void deleteAllMessage() {
+  public void deleteAllMessages() {
     messageService.deleteAllMessages();
   }
 
   @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
   public void deleteMessageById(@PathVariable String id) {
-    messageRepository.deleteById(new ObjectId(id));
+    messageService.deleteMessageById(id);
   }
 
 }
