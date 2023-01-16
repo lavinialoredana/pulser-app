73402bd0bba3ce0b6f6b309a91a4b0aaff07280e
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-16 17:33:05 +0100

    PULSER-2: change back face type to enum.

--- src/test/java/com/thoughtworks/pulser/service/MessageServiceTest.java
+++ src/test/java/com/thoughtworks/pulser/service/MessageServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 
 import com.thoughtworks.pulser.model.Message;
+import com.thoughtworks.pulser.model.Message.Face;
 import com.thoughtworks.pulser.repository.MessageRepository;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -16,8 +17,8 @@ public class MessageServiceTest {
 
   private MessageRepository messageRepository = mock(MessageRepository.class);
   private MessageService messageService = new MessageService(messageRepository);
-  private Message message = new Message("HAPPY", "my message");
-  private Message message2 = new Message("AWESOME", "my second message");
+  private Message message = new Message(Face.HAPPY, "my message");
+  private Message message2 = new Message(Face.AWESOME, "my second message");
 
   @Test
   void itShouldSaveMessage() {
@@ -57,14 +58,14 @@ public class MessageServiceTest {
 
   @Test(expectedExceptions = InputMismatchException.class, expectedExceptionsMessageRegExp = "ERROR: Empty message body given")
   void itShouldThrowsExceptionWhenEmptyBodyMessage() {
-    Message emptyBodyMessage = new Message("AWESOME", null);
+    Message emptyBodyMessage = new Message(Face.AWESOME, null);
 
     messageService.validateMessage(emptyBodyMessage);
   }
 
   @Test(expectedExceptions = InputMismatchException.class, expectedExceptionsMessageRegExp = "ERROR: Message body given too long. It should be less than 1400 chars")
   void itShouldThrowsExceptionWhenBodyMessageLengthIsGreaterThan1400Chars() {
-    Message longBodyMessage = new Message("AWESOME",
+    Message longBodyMessage = new Message(Face.AWESOME,
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ultricies ante non malesuada. Fusce ullamcorper interdum augue, et laoreet dolor euismod ut. Ut dictum ante eros, et dapibus urna vestibulum vel. Suspendisse eget diam mi. Donec consequat diam justo, eu interdum nisi laoreet elementum. Mauris consequat suscipit viverra. Suspendisse ac porta neque.Pellentesque aliquam vel metus sagittis blandit. Vivamus ut ipsum vel ex efficitur vestibulum eget ac felis. Vestibulum sagittis ac lacus sed tristique. Integer vulputate orci ut ornare commodo. Morbi eleifend consequat euismod. Sed sagittis leo enim, sit amet porttitor quam auctor in. Sed quis ultrices leo. Nullam posuere fringilla tortor, in aliquet urna viverra convallis. Integer sollicitudin nunc vel nisi placerat, non vestibulum lacus rutrum. Proin venenatis metus sit amet fermentum ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed maximus cursus massa, vitae ullamcorper ipsum fermentum eget. Sed fermentum massa eleifend scelerisque tempor.Nullam commodo eget dolor eget ultricies. Suspendisse at ornare tellus. Sed nec tincidunt erat, eget tincidunt tortor. Sed efficitur turpis nec diam iaculis, a volutpat purus tristique. Suspendisse et iaculis mi. Pellentesque semper lorem eget tortor vulputate, a dignissim nulla efficitur. Phasellus eu risus iaculis nullamj.");
 
     messageService.validateMessage(longBodyMessage);
