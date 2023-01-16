c2274bab798a0564180b10968b7659f8e34d030f
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-16 17:31:30 +0100

    PULSER-2: change back face type to enum. Change Id type to String

--- src/main/java/com/thoughtworks/pulser/model/Message.java
+++ src/main/java/com/thoughtworks/pulser/model/Message.java
@@ -1,30 +1,33 @@
 package com.thoughtworks.pulser.model;
 
 import java.time.LocalDateTime;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "messages")
 public class Message {
 
+  public enum Face {
+    AWESOME,
+    HAPPY,
+    SAD,
+    AWFUL
+  }
   @Id
-  private ObjectId id; //TODO @Rebeca: Just a String would do the trick instead of ObjectId
-  private String face; //TODO @Rebeca: Consider using an enum here so you can have full control of what goes into a "face"
+  private String id;
+  private Face face;
   private String body;
-  LocalDateTime creationTimeStamp; //TODO @Rebeca: createdDate is more typical
+  LocalDateTime createDate;
 
-  //TODO @Rebeca: You are lacking the like, dislike and report counters here
-
-  public Message(String face, String body) {
+  public Message(Face face, String body) {
     this.face = face;
     this.body = body;
-    this.creationTimeStamp = LocalDateTime.now();
+    this.createDate = LocalDateTime.now();
   }
 
   public String getBody() { return body; }
 
-  public ObjectId getId() {
+  public String getId() {
     return id;
   }
 
