9232c4beb4a40bff89f9eda83067229377738bce
Rebeca Gonzalez <bequipedia@gmail.com>
2023-01-16 17:19:06 +0100

    PULSER-2: change Id type to String

--- src/main/java/com/thoughtworks/pulser/repository/MessageRepository.java
+++ src/main/java/com/thoughtworks/pulser/repository/MessageRepository.java
@@ -1,11 +1,10 @@
 package com.thoughtworks.pulser.repository;
 
 import com.thoughtworks.pulser.model.Message;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends MongoRepository<Message, ObjectId> { //TODO @Rebeca: <Message, String>
+public interface MessageRepository extends MongoRepository<Message, String> {
 
 }
