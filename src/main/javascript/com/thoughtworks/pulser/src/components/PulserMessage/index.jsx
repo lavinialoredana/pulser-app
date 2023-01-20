ca055af41882ce28eb8b6afed32b13620d112746
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-01-20 13:09:52 +0100

    Small refactor and apply  styling to PulserMessage

--- src/main/javascript/com/thoughtworks/pulser/src/components/PulserMessage/index.jsx
+++ src/main/javascript/com/thoughtworks/pulser/src/components/PulserMessage/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import happyFace from "../../assets/images/happy_face.png";
+import "./PulserMessage.css";
 
 const PulserMessage = () => {
   return (
-    <div className="Pulser-message">
-      <div className="Title-image-reaction">
+    <div className="Pulser-message-container">
+      <div className="Title-image-reaction-container">
         <div className="Reaction-title" data-testid="reaction-title">
-          <span> Happy</span>
+          <h3> Happy</h3>
         </div>
-        <div className="Reaction-image">
-          <img src={happyFace} alt="happy_face" />
+        <div>
+          <img src={happyFace} alt="happy_face" className="Reaction-image" />
         </div>
       </div>
 
       <div className="Reaction-message" data-testid="reaction-message">
-        <p> First message </p>
+        <p>First message</p>
       </div>
     </div>
   );
